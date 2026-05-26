05fc87d547d4bc17f084841c8a091ff0d0adf6c1
Kosma Gustowski <gustowskikosma@onet.pl>
2026-05-26 22:54:31 +0200

    speed

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Footer from "./components/layout/footer";
 import Topka from "./components/layout/topka";
-import { Analytics } from "@vercel/analytics/next"
-
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -37,6 +37,7 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col">
         <Topka />
 <Analytics/>
+ <SpeedInsights/>
         <main className="flex-1">{children}</main>
 
         <Footer />
