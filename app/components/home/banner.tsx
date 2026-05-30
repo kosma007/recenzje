48281a6cb371aa9549600e84513060ce7263ba0c
Kosma Gustowski <gustowskikosma@onet.pl>
2026-05-30 23:26:58 +0200

    dsa

--- app/components/home/banner.tsx
+++ app/components/home/banner.tsx
@@ -5,14 +5,14 @@ import Link from "next/link";
 
 export default function Banner() {
   return (
-    <div className="relative max-w-6xl h-[200px] lg:h-[490px] rounded-xl overflow-hidden border border-zinc-800 mb-8 mt-10 ">
+    <div className="relative max-w-6xl h-[200px] lg:h-[430px] rounded-xl overflow-hidden border border-zinc-800 mb-8 mt-10 ">
 
       {/* BACKGROUND IMAGE */}
       <Image
         src="/banner2.png" // wrzuć swój obraz do /public
         alt="banner"
         fill
-        className="object-fill lg:object-cover "
+        className="object-fill lg:object-cover object-center"
         priority
       />
 
